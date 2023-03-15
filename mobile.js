cd62adf200d943d585a0183be84616fcb5cc5707
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-03-15 20:09:43 +0200

    Make changes on CSS and HTML and add JS file

---
+++ mobile.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.querySelector('.body');
+const close = document.querySelector('.close');
+const toolbarMobileMenu = document.querySelector('.toolbar-mobile-menu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden'); // shows the mobile menu
+  // hamburger.classList.toggle('hidden'); // hides the hamburger icon
+  body.classList.toggle('overflow');
+});
+
+close.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  body.classList.toggle('overflow');
+});
+
+toolbarMobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  body.classList.toggle('overflow');
+});
