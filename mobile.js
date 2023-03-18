9fbd25ba2c2fab333ea90ecb2c7d18a021c9236d
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-03-18 19:44:48 +0200

    Make changes

--- mobile.js
+++ mobile.js
@@ -3,10 +3,43 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const body = document.querySelector('.body');
 const close = document.querySelector('.close');
 const toolbarMobileMenu = document.querySelector('.toolbar-mobile-menu');
+const projects = [
+  {
+    image: 'photo/Snapshoot Portfolio (1).png',
+    name: 'components 1',
+    languages: ['HTML', 'CSS', 'JavaScript', 'RUby', 'Bootstrap', 'Javascript', 'Bootstrap'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit  culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Qui  s, quos repellendus.',
+    live: 'https://khaled-alaa-1.github.io/Portfolio-setup/',
+    github: 'https://github.com/Khaled-Alaa-1',
+  },
+  {
+    image: 'photo/Snapshoot Portfolio (2).png',
+    name: 'components 2',
+    languages: ['HTML', 'CSS', 'JavaScript', 'RUby', 'Bootstrap', 'Javascript', 'Bootstrap'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit  culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Qui  s, quos repellendus.',
+    live: 'https://khaled-alaa-1.github.io/Portfolio-setup/',
+    github: 'https://github.com/Khaled-Alaa-1',
+  },
+  {
+    image: 'photo/Snapshoot Portfolio (3).png',
+    name: 'components 3',
+    languages: ['HTML', 'CSS', 'JavaScript', 'RUby', 'Bootstrap', 'Javascript', 'Bootstrap'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit  culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Qui  s, quos repellendus.',
+    live: 'https://khaled-alaa-1.github.io/Portfolio-setup/',
+    github: 'https://github.com/Khaled-Alaa-1',
+  },
+  {
+    image: 'photo/Snapshoot Portfolio.png',
+    name: 'components 4',
+    languages: ['Ruby on Rails', 'css', 'JavaScript', 'Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit  culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Qui  s, quos repellendus.',
+    live: 'https://khaled-alaa-1.github.io/Portfolio-setup/',
+    github: 'https://github.com/Khaled-Alaa-1',
+  },
+];
 
 hamburger.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden'); // shows the mobile menu
-  // hamburger.classList.toggle('hidden'); // hides the hamburger icon
   body.classList.toggle('overflow');
 });
 
@@ -14,52 +47,61 @@ close.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
   body.classList.toggle('overflow');
 });
-
 toolbarMobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
   body.classList.toggle('overflow');
 });
-{ <div class="popup-window">
-  <img class="pop-close-desk" src="images/icons/pop-close-desk.svg" alt="pop-close-desk">
-  <div class="pop-image-and-close">
-    <img src=${projects[i].image} alt="exit" class="pop-close hidden"></img>
+
+// see project ////////////////////////////////////////////
+const seeProject = document.querySelectorAll('.Project-link');
+const popup = document.querySelector('.popup');
+
+for (let i = 0; i < 4; i += 1) {
+  const html = `    <div class="popup-1">
+  <div class="pop-head">
+    <h2>${projects[i].name}</h2>
+<img class="exit" src="photo/x.png" alt="">  
+</div>
+  <ul class="Project-list">
+    <li class="Project-name">CANOPY</li>
+    <li class="dot">&#8226;</li>
+    <li class="Project-info">Back End Dev</li>
+    <li class="dot">&#8226;</li>
+    <li class="Project-info">2015</li>
+  </ul>
+  <div class="Tonic-img">
+    <img  class="Project-img" src="${projects[i].image}" alt="Snapshoot Tonic">
+    <img class="Project-img-2" src="photo/work-1.png" alt=" Work Tonic">
   </div>
-  <div class="pop-project-name">${projects[i].name}</div>
-  <div class="pop-project-name-and-live-and-source-desk">
-    <div class="pop-project-name-desk">${projects[i].name}</div>
-    <div class="pop-see-live-and-source-desk">
-      <a href=${projects[i].live} class="pop-see-live">
-        <h3 class="pop-see-live-text">See live</h3>
-        <img src="images/icons/see-live.svg" alt="" class="pop-see-live-image">
-      </a>
-      <a href=${projects[i].github} class="pop-see-source">
-        <h3 class="pop-see-source-text">See Source</h3>
-        <img src="images/icons/see-source.svg" alt="" class="pop-see-source-image">
-      </a>
-    </div>
+  <div class="pop">
+  <p class="text-pop">${projects[i].description}</p>
+  <div class="pop-lan">
+    <ul class="Links-list">
+      <li class="links-1"><a href="">${projects[i].languages[1]}</a></li>
+      <li class="links-2"><a href="">${projects[i].languages[2]}</a></li>
+      <li class="links-3"><a href="">${projects[i].languages[3]}</a></li>
+      </ul>
+      <ul class="Links-list-2">
+      <li class="links-4"><a href="">${projects[i].languages[4]}</a></li>
+      <li class="links-5"><a href="">${projects[i].languages[5]}</a></li>
+    </ul>
+    <br>
+    <ul class="button-pop">
+      <li class="button"><button href=${projects[i].live}>See live <img src="photo/see live.png" alt=""></button></li>
+      <li class="button"><button href=${projects[i].github}>See source <img src="photo/Vector.png" alt=""></button></li>
+    </ul>
   </div>
-  <div class="pop-languages">
-    <h4 class="pop-languages-1">${projects[i].languages[0]}</h4>
-    <h4 class="pop-languages-2">${projects[i].languages[1]}</h4>
-    <h4 class="pop-languages-3">${projects[i].languages[2]}</h4>
   </div>
-  <div class="pop-languages-desk">
-    <h4 class="pop-languages-1 desk">${projects[i].languages[3]}</h4>
-    <h4 class="pop-languages-2 desk">${projects[i].languages[4]}</h4>
-    <h4 class="pop-languages-3 desk">${projects[i].languages[5]}</h4>
-    <h4 class="pop-languages-1 desk">${projects[i].languages[6]}</h4>
-    <h4 class="pop-languages-2 desk">${projects[i].languages[7]}</h4>
-    <h4 class="pop-languages-3 desk">${projects[i].languages[8]}</h4>
-  </div>
-  <p class="pop-lorem">${projects[i].description}</p>
-  <div class="pop-see-live-and-source mobile">
-    <a href=${projects[i].live} class="pop-see-live">
-      <h3 class="pop-see-live-text">See live</h3>
-      <img src="images/icons/see-live.svg" alt="" class="pop-see-live-image">
-    </a>
-    <a href=${projects[i].github} class="pop-see-source">
-      <h3 class="pop-see-source-text">See Source</h3>
-      <img src="images/icons/see-source.svg" alt="" class="pop-see-source-image">
-    </a>
-  </div>
-  </div> }
+</div>`;
+  seeProject[i].addEventListener('click', () => {
+    const main = document.querySelector('.popup');
+    main.innerHTML = html;
+    main.classList.toggle('blur');
+    const popClose = document.querySelector('.exit');
+    const popupWindow = document.querySelector('.popup-1');
+    popClose.addEventListener('click', () => {
+      popupWindow.classList.toggle('hidden');
+      main.classList.toggle('blur');
+    });
+  });
+}
